4c88fa995f10902fca78da76af140bade94f2ba5
husain <HUSAINS993@GMAIL.COM>
2018-10-23 11:31:00 +0530

    initial commit

---
+++ custom-js.js
@@ -0,0 +1,11 @@
+var api_path = "api.openweathermap.org/data/2.5/weather?q=";
+var city = input.value;
+var metric = "&units=metric";
+var url = api_path + city + metric;
+var location;
+
+window.onload(function(){
+	location = navigator.
+
+
+})
